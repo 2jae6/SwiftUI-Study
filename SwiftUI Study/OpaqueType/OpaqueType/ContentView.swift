f80ea2fc73785fad331608c17b5e1f78e42db05e
JaeWook Lee <2jae66@gmail.com>
2020-09-21 20:49:03 +0900

    OpaqueType

---
+++ SwiftUI Study/OpaqueType/OpaqueType/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OpaqueType
+//
+//  Created by 1 on 2020/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: Text {
+        Text("Hello, world!")
+          
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

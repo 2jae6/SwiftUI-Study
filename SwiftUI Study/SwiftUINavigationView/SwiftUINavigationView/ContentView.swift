8ffcc686fa6985abde093feb9cc1e8ec4f92dc4d
JaeWook Lee <2jae66@gmail.com>
2020-11-06 23:56:16 +0900

    swiftui navigation

--- SwiftUI Study/SwiftUINavigationView/SwiftUINavigationView/ContentView.swift
+++ SwiftUI Study/SwiftUINavigationView/SwiftUINavigationView/ContentView.swift
@@ -9,8 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+      
+            
+            let leadingItem = Button(action: {
+                print("Leading item tapped")
+            }){
+                Image(systemName: "bell").imageScale(.large)
+            }
+            
+            let trailingItem = Button(action: {
+                print("Trailing")
+            }){
+                Image(systemName: "gear")
+                    .imageScale(.large)
+            }
+        return   NavigationView{
+            Image("SwiftUI")
+                .navigationBarItems(leading: leadingItem, trailing: trailingItem)
+                .navigationBarTitle("네비게이션 바 타이틀")
+        }
         
-        Image("SwiftUI")
     }
 }
 

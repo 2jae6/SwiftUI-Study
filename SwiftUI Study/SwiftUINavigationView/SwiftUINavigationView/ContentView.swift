66a36211baa0ebc19a73d45e5ad6a44145040385
JaeWook Lee <2jae66@gmail.com>
2020-11-06 09:49:59 +0900

    swiftUI Navigation

---
+++ SwiftUI Study/SwiftUINavigationView/SwiftUINavigationView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUINavigationView
+//
+//  Created by 1 on 2020/11/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        Image("SwiftUI")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
